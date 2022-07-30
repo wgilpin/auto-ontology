1295d2637c956e55795ba48885702efe254f1396
Will Gilpin <wgilpin@gmail.com>
2022-07-30 15:54:59 +0100

    model builder

--- model_k.py
+++ model_k.py
@@ -1,48 +1,15 @@
 # %%
+from typing import Any
 import tensorflow as tf
 
 from tensorflow import keras as k
 from tensorflow.keras.layers import Dense, Input
 
-# %%
-
-
-def build_model(input_dims, latent_dims):
-
-    input_layer = k.layers.Input(shape=(input_dims,))
-
-    # Encoder
-    e1 = k.layers.Dense(input_dims*3/4, name="encoder_input")(input_layer)
-    e2 = k.layers.Dense(input_dims/2, name="encoder_hidden")(e1)
-    encoder = k.layers.Dense(latent_dims, name="encoder_output")(e2)
-
-    # Decoder
-    d1 = k.layers.Dense(input_dims/2, name="decoder_input")(encoder)
-    d2 = k.layers.Dense(input_dims*3/4, name="decoder_hidden")(d1)
-    decoder_logits = k.layers.Dense(input_dims, name="decoder_output")(d2)
-    decoder = tf.nn.sigmoid(decoder_logits, name="decoder_sigmoid")
-
-    # Latent network
-    # l1 = k.layers.Dense(input_dims*3/2)(decoder)
-    # l2 = k.layers.Dense(input_dims/2)(l1)
-    # latent = k.layers.Dense(latent_dims)(l2)
-
-    model = k.Model(inputs=input_layer, outputs=[decoder])
-
-    return model
-
-
-def compile_ae_model():
-    optimizer = k.optimizers.Adam(lr=0.001, decay=1e-6)
-    ae_model = build_model(input_dims=768, latent_dims=128)
-    ae_model.compile(optimizer, loss="mse")
-    print(ae_model.summary())
-    return ae_model
 
 # %%
 
-
-def create_layer(config: dict) -> Dense:
+# create a Dense layer from a config
+def create_layer(config: dict, name: str) -> Dense:
     """
     Create a layer from a config dictionary.
     - config: dictionary of layer parameters
@@ -50,6 +17,7 @@ def create_layer(config: dict) -> Dense:
         - act: activation function
     """
     return Dense(
+        name=name,
         units=config["n"],
         activation=config["act"],
         kernel_regularizer='l1')
@@ -57,10 +25,18 @@ def create_layer(config: dict) -> Dense:
 # %%
 
 
-def create_models_from_params(layers, latent_layer, output_fn, optimizer_fn, loss_fn, verbose=1):
+# %%
+
+# build the autoencoder
+
+
+def create_ae_encoder_model(
+        emb_size: int,
+        layers: list[dict],
+        latent_layer: int) -> k.Model:
     """
     Create a model with the given parameters.
-    - input_dims: the number of input dimensions
+    - emb_size: the number of input dimensions
     - latent_layer: latent layer config: dict with keys:
           - n: int number of units
           - act: str activation function
@@ -68,42 +44,102 @@ def create_models_from_params(layers, latent_layer, output_fn, optimizer_fn, los
         array of dict with keys:
           - n: int number of units
           - act: str activation function
-    - optimizer: the optimizer to use
-    - loss: the loss function to use
     """
 
     # build ml, the array of layers
-    ml = [Input(shape=(layers[0]["n"],))]
-    for l in layers:
-        ml.append(create_layer(l)(ml[-1]))
-    ml.append(create_layer(latent_layer)(ml[-1]))
-    for _, l in enumerate(reversed(layers[1:]), 1):
-        ml.append(create_layer(l)(ml[-1]))
+    ml = [Input(shape=(emb_size,))]
+    for n, l in enumerate(layers):
+        ml.append(create_layer(l, f"encoder_{n}")(ml[-1]))
+    ml.append(create_layer(latent_layer, "ae_latent")(ml[-1]))
+
+    return ml
+
+
+def create_ae_decoder_model(
+        input_layer: Dense,
+        layers: list[dict],
+        output_fn) -> k.Model:
+    """
+    Create a model with the given parameters.
+    - layers: list of layers in the encoder. Reverse order.
+        array of dict with keys:
+          - n: int number of units
+          - act: str activation function
+    """
+
+    # build ml, the array of layers
+    ml = [create_layer(layers[0], "decoder_in")(input_layer)]
+    for n, l in enumerate(reversed(layers[1:]), 1):
+        ml.append(create_layer(l, f"decoder_{n}")(ml[-1]))
 
     out_config = {**layers[0], "act": output_fn}
-    ml.append(create_layer((out_config))(ml[-1]))
-
-    model = k.Model(inputs=ml[0], outputs=[ml[-1]])
-
-    model.compile(optimizer_fn, loss_fn)
-    if verbose:
-        print(model.summary())
-    return model
+    ml.append(create_layer(out_config, "decoder_out")(ml[-1]))
+    return ml
 
 # %%
+# create the latent_space model
 
-# layers1 = [
-#     {"n": 768, "act": "relu"},
-#     {"n": 400, "act": "relu"},
-# ]
-# latent_layer1 = {"n": 128, "act": "relu"}
-# output_fn1 = "sigmoid"
-# optimizer_fn1 = k.optimizers.Adam(lr=0.001, decay=1e-6)
-# loss_fn1 = "mse"
 
-# model1 = create_models_from_params(layers1, latent_layer1, output_fn1, optimizer_fn1, loss_fn1)
-# model1.summary()
-# model1.compile(optimizer_fn1, loss_fn1)
+def create_latent_space_model(
+        input_layer: Dense,
+        layers: list[dict],
+        output_fn: Any) -> k.Model:
+    """
+    Create a model with the given parameters.
+    - input_layer: previous model output layer
+    - layers: list of layers in the encoder. Decoder is mirrored.
+        array of dict with keys:
+          - n: int number of units
+          - act: str activation function
+    - output_fn: the output function to use
+    """
+    # build ml, the array of layers
+    ml = [input_layer]
+    for n, l in enumerate(layers):
+        ml.append(create_layer(l, f"latent_{n}")(ml[-1]))
+
+    out_config = {**layers[0], "act": output_fn}
+    ml.append(create_layer(out_config, "latent_out")(ml[-1]))
+    return ml
+
+# %%
+# CREATE THE MODEL
+
+
+def create_model(cfg: dict, emb_size: int = 768, verbose: int = 0) -> k.Model:
+    """
+    Create a model with the given parameters.
+    -cfg: the model configuration. Dict with keys:
+        - latent: latent layer
+        - ae: autoencoder layers
+            array of dict with keys:
+            - n: int number of units
+            - act: str activation function
+        - emb_size: the number of input dimensions
+    """
+    enc = create_ae_encoder_model(
+        emb_size=emb_size,
+        layers=cfg['ae'],
+        latent_layer=cfg['latent'])
+    dec = create_ae_decoder_model(
+        input_layer=enc[-1],
+        layers=cfg['ae'],
+        output_fn=cfg['output'])
+    latent_space = create_latent_space_model(
+        input_layer=enc[-1],
+        layers=cfg['ae'],
+        output_fn='sigmoid')
+    full_model = k.Model(
+        inputs=enc[0],
+        outputs=[
+            dec[-1],
+            latent_space[-1],
+            ])
+
+    full_model.compile(cfg['opt'], cfg['loss'])
+    if verbose:
+        print(full_model.summary())
+    return full_model
 
 
 # %%
