c9a446966fd743c2df851a41921b3e319cf967c3
Will Gilpin <wgilpin@gmail.com>
2022-08-01 15:30:30 +0100

    train on fewNERD too

--- project.py
+++ project.py
@@ -19,101 +19,12 @@ from timer import timer
 
 import matplotlib.pyplot as plt
 
-from spacyNER import get_spacy_NER_data
+from spacyNER import create_training_data_per_entity_spacy
 
 
 # %%
 
-emb_pipe = make_pipe('distilbert-base-uncased', TFDistilBertModel)
 
-early_stopping_cb = EarlyStopping(
-    monitor='loss', patience=3, verbose=1, min_delta=0.001)
-
-# %%
-
-# test
-def test():
-    z = ['The brown fox jumped over the dog',
-         'The ship sank in the Atlantic Ocean',
-         'Urban foxes sometimes fight with dogs']
-    print("Start")
-    embedding_features1 = embed(emb_pipe, z[0])
-    print(
-        f"emb shape is {embedding_features1.shape} for {len(z[0].split(' '))} words")
-    embedding_features2 = embed(emb_pipe, z[1])
-    embedding_features3 = embed(emb_pipe, z[2])
-    distance12 = 1-cosine(embedding_features1[0], embedding_features2[0])
-    distance23 = 1-cosine(embedding_features2[0], embedding_features3[0])
-    distance13 = 1-cosine(embedding_features1[0], embedding_features3[0])
-    print(f"distance 1-2 {distance12}")
-    print(f"distance 2-3 {distance23}")
-    print(f"distance 1-3 {distance13}")
-
-# %%
-# create 1 training item per entity, given a token radius
-
-@timer
-def create_training_data_per_entity(
-        length: int = 10,
-        radius: int = 7) -> list:
-    """
-    Creates training data for the autoencoder.
-    """
-    from tqdm import tqdm
-
-    print(f"Create Training Data for {length} items, radius {radius}")
-    # get spacy data
-    spacy_data = get_spacy_NER_data(length)
-    print(f"Created NER Data for {len(spacy_data)} items")
-
-    # get embedding data
-    res = []
-    with tqdm(total=len(spacy_data)) as pbar:
-        for s in spacy_data:
-            for chunk in s["chunks"]:
-                start_token = max(chunk["entity"].start-radius, 0)
-                end_token = min(chunk["entity"].end+radius, len(s["nlp"]))
-                short_sentence = s["nlp"][start_token:end_token]
-                res.append({
-                    "sentence": short_sentence,
-                    "chunk": str(chunk["chunk"]),
-                    "label": chunk["entity"].label,
-                    "label_id": chunk["entity"].label_,
-                    "embedding": embed(emb_pipe, str(short_sentence))
-                })
-                pbar.update(1)
-    # average all the embeddings in a sample, dropping 1st (CLS)
-    for r in res:
-        r["embedding"] = np.mean(r["embedding"][1:], axis=0)
-    return res
-
-
-
-# %%
-# 
-# CREATE TRAINING DATA
-merged = create_training_data_per_entity(-1, 5)
-print(len(merged))
-
-
-# %%
-# create full DF
-
-x_train_np = np.stack([m["embedding"] for m in merged])
-
-merged_df = pd.DataFrame(merged)
-emb_df = pd.DataFrame(x_train_np)
-
-merged_df.drop(columns=['embedding'], inplace=True)
-df = pd.concat([merged_df, emb_df], axis=1)
-
-df['label'] = np.unique(df['label'], return_inverse = True)[1]
-
-
-print(df.shape)
-# save data to csv
-df.to_csv("./data/training_df.csv", index=False)
-df.head()
 
 # %% [markdown]
 # # IMPORT import df from file
@@ -121,25 +32,17 @@ df.head()
 
 # %%
 import pandas as pd
+from data import get_training_data
 
-df = pd.read_csv("./data/training_df.csv")
-# train df has only the embeddings
+# train_x/y has only the embeddings
+train_x, test_x, train_y, test_y = get_training_data(
+    save_dir="results",
+    radius=10,
+    fraction=0.99,
+    count=-1,
+    force_recreate=True,
+    entity_filter=['PER','ORG','LOC'])
 
-def test_train_split(frac=0.8):
-    training_data = df.sample(frac=frac, random_state=42)
-    testing_data = df.drop(training_data.index)
-    train_y_df = np.array(training_data["label"])
-    test_y_df = np.array(testing_data["label"])
-    train_x_df = np.array(training_data.drop(columns=['chunk', 'label', 'label_id', 'sentence']))
-    test_x_df = np.array(testing_data.drop(columns=['chunk', 'label', 'label_id', 'sentence']))
-    print(f"Full df: {df.shape}")
-    print(f"Train df: {train_x_df.shape}")
-    print(f"Test df: {test_x_df.shape}")
-    return train_x_df, test_x_df, train_y_df, test_y_df
-
-
-# %%
-train_x, test_x, train_y, test_y = test_train_split(0.8)
 
 # %%
 # plot loss
@@ -341,6 +244,9 @@ from keras.initializers import VarianceScaling
 from tensorflow.keras.optimizers import SGD
 from DEC import DEC
 from metrics import acc
+from data import get_training_data
+
+entity_types=['PER','ORG','LOC', 'MISC']
 
 save_dir = "./results"
 if not os.path.exists(save_dir):
@@ -349,46 +255,76 @@ if not os.path.exists(save_dir):
 # load dataset
 dataset = "conll"
 if dataset == "reuters":
-    from datasets import load_reuters
+    from datasets_deep import load_reuters
     x, y = load_reuters()
 else:
-    x, test_x, y, test_y = test_train_split(0.99)
-print(f"{dataset} dataset: {x.shape} x:{type(x)}, y:{type(y)}")
-n_clusters = len(np.unique(y))
-print(f"{n_clusters} clusters")
-
-init = 'glorot_uniform'
-pretrain_optimizer = 'adam'
-# setting parameters
-
-update_interval = 30
-pretrain_epochs = 50
-init = VarianceScaling(scale=1. / 3., mode='fan_in',
-                        distribution='uniform')  # [-limit, limit], limit=sqrt(1./fan_in)
-pretrain_optimizer = SGD(lr=1, momentum=0.9)
+    x, test_x, y, test_y = get_training_data(save_dir="results",
+                                             count=10000,
+                                             source="fewNERD",
+                                             radius=5,
+                                             fraction=0.99,
+                                             entity_filter=entity_types,
+                                             force_recreate=False)
+x.shape
 
 # %%
-# prepare the DEC model
-dec = DEC(dims=[x.shape[-1], 500, 500, 2000, 10], n_clusters=n_clusters, init=init)
+def train_DEC(x, y):
+    print(f"{dataset} dataset: {x.shape} x:{type(x)}, y:{type(y)}")
+    assert(len(np.unique(y)) <= len(entity_types))
+    n_clusters = len(np.unique(y))
+    print(f"{n_clusters} clusters")
 
-if os.path.exists(os.path.join(save_dir, 'ae_weights.h5')):
-    print("Loading weights")
-    dec.autoencoder.load_weights(os.path.join(save_dir, 'ae_weights.h5'))
-else:
-    print("Training weights")
-    dec.pretrain(x=x, y=y, optimizer=pretrain_optimizer,
-                    epochs=pretrain_epochs, batch_size=256,
-                    save_dir='./results')
+    init = 'glorot_uniform'
+    pretrain_optimizer = 'adam'
+    # setting parameters
 
-dec.model.summary()
-dec.compile(optimizer=SGD(0.01, 0.9), loss='kld')
+    pretrain_epochs = 50
+    init = VarianceScaling(scale=1. / 3., mode='fan_in',
+                        distribution='uniform')  # [-limit, limit], limit=sqrt(1./fan_in)
+    pretrain_optimizer = SGD(learning_rate=1, momentum=0.9)
 
+    # prepare the DEC model
+    dec = DEC(dims=[x.shape[-1], 500, 500, 2000, 10], n_clusters=n_clusters, init=init)
+
+    if os.path.exists(os.path.join(save_dir, 'ae_weights.h5')):
+        print("Loading weights")
+        dec.autoencoder.load_weights(os.path.join(save_dir, 'ae_weights.h5'))
+    else:
+        print("Training weights")
+        dec.pretrain(x=x, y=y, optimizer=pretrain_optimizer,
+                        epochs=pretrain_epochs, batch_size=256,
+                        save_dir='./results')
+
+    dec.model.summary()
+    dec.compile(optimizer=SGD(0.01, 0.9), loss='kld')
+
+    return dec
+
+dec = train_DEC(x, y)
 
 # %%
 %%timeit 
+update_interval = 30
 y_pred = dec.fit(x, y=y, tol=0.001, maxiter=2e4, batch_size=256,
     update_interval=update_interval, save_dir=save_dir)
 print('acc:', acc(y, y_pred))
+
+
+# %% [markdown]
+# 
+# # fewNERD data
+
+# %%
+import numpy as np
+from timer import timer
+from fewNERD_data import create_training_data_per_entity_fewNERD
+
+
+# %%
+training_data = create_training_data_per_entity_fewNERD(length=50, radius=10, entity_filter=[1,2,3,4])
+print("Training data created")
+
+# %%
 
 
 
