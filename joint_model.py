5ba2692213395334e7281edccdf03adad14c0f4d
Will Gilpin <wgilpin@gmail.com>
2022-08-17 17:13:21 +0100

    tidying up

--- joint_model.py
+++ joint_model.py
@@ -25,43 +25,51 @@ print(x.shape)
 
 # %%
 
-def autoencoder_model(dims, act='relu', init='glorot_uniform'):
+def autoencoder_model(layer_specs: list, act: str='relu', init_fn: str='glorot_uniform'):
     """
-    Fully connected auto-encoder model, symmetric.
-    Arguments:
-        dims: list of number of units in each layer of encoder. dims[0] is
-            input dim, dims[-1] is units in hidden layer.
-            The decoder is symmetric with encoder. So number of layers of
-            the auto-encoder is 2*len(dims)-1
-        act: activation, not applied to Input, Hidden and Output layers
-    return:
-        (ae_model, encoder_model), Model of autoencoder and model of encoder
+    Creates the autoencoder given
+    -layer_specs: list of layer sizes.
+        Model is symmetrical so only need to specify the first half.
+    -act: activation function for hidden layers
+    -init_fn: initializer for weights
+
+    returns:
+        - the full autoencoder
+        - the encoder only
     """
-    n_stacks = len(dims) - 1
+    layers = len(layer_specs) - 1
     # input
-    input_img = Input(shape=(dims[0],), name='input')
+    input_img = Input(shape=(layer_specs[0],), name='input')
     x = input_img
-    # internal layers in encoder
-    for i in range(n_stacks-1):
-        x = Dense(dims[i + 1], activation=act,
-                  kernel_initializer=init, name=f'encoder_{i}')(x)
 
-    # hidden layer
-    # hidden layer, features are extracted from here
-    encoded = Dense(dims[-1], kernel_initializer=init,
-                    name=f'encoder_{n_stacks - 1}')(x)
+    # hidden layers in encoder
+    for i in range(layers-1):
+        x = Dense(
+            layer_specs[i + 1],
+            activation=act,
+            kernel_initializer=init_fn,
+            name=f'encoder_{i}')(x)
 
-    x = encoded
-    # internal layers in decoder
-    for i in range(n_stacks-1, 0, -1):
-        x = Dense(dims[i], activation=act,
-                  kernel_initializer=init, name=f'decoder_{i}')(x)
+    # latent layer
+    encoder = Dense(
+        layer_specs[-1],
+        kernel_initializer=init_fn,
+        name=f'encoder_{layers - 1}')(x)
+
+    x = encoder
+    # hidden layers in decoder
+    for i in range(layers-1, 0, -1):
+        x = Dense(
+            layer_specs[i],
+            activation=act,
+            kernel_initializer=init_fn,
+            name=f'decoder_{i}')(x)
 
     # output
-    x = Dense(dims[0], kernel_initializer=init, name='decoder_0')(x)
-    decoded = x
-    return (Model(inputs=input_img, outputs=decoded, name='AE'),
-            Model(inputs=input_img, outputs=encoded, name='encoder'))
+    x = Dense(layer_specs[0], kernel_initializer=init_fn, name='decoder_0')(x)
+    decoder = x
+    return (Model(inputs=input_img, outputs=decoder, name='AE'),
+            Model(inputs=input_img, outputs=encoder, name='encoder'))
 
 
 
@@ -76,8 +84,10 @@ metrics.acc(y, y_pred_kmeans)
 
 # %%
 dims = [x.shape[-1], 500, 500, 2000, 50]
-init = VarianceScaling(scale=1. / 3., mode='fan_in',
-                       distribution='uniform')
+init = VarianceScaling(
+                    mode='fan_in',
+                    scale=1. / 3.,
+                    distribution='uniform')
 pretrain_optimizer = SGD(lr=1, momentum=0.9)
 pretrain_epochs = 300
 batch_size = 256
@@ -89,28 +99,26 @@ save_dir = './results'
 
 class ClusteringLayer(Layer):
     """
-    Clustering layer converts input sample (feature) to soft label, i.e. a vector that represents the probability of the
-    sample belonging to each cluster. The probability is calculated with student's t-distribution.
+    Clustering layer predicts the cluster assignments for each sample in the batch.
+    Calculated with student's t-distribution.
 
     # Example
     ```
-        model.add(ClusteringLayer(n_clusters=10))
+        model.add(ClusteringLayer(num_clusters=10))
     ```
     # Arguments
-        n_clusters: number of clusters.
-        weights: list of Numpy array with shape `(n_clusters, n_features)` witch represents the initial cluster centers.
-        alpha: degrees of freedom parameter in Student's t-distribution. Default to 1.0.
-    # Input shape
-        2D tensor with shape: `(n_samples, n_features)`.
-    # Output shape
-        2D tensor with shape: `(n_samples, n_clusters)`.
+        num_clusters: number of clusters.
+        weights: list of Numpy array with shape `(n_clusters, n_features)` which
+            represents the initial cluster centers from pretraining.
+        alpha: degrees of freedom parameter in Student's t-distribution.
+            Default to 1.0.
     """
 
-    def __init__(self, n_clusters, weights=None, alpha=1.0, **kwargs):
+    def __init__(self, num_clusters, weights=None, alpha=1.0, **kwargs):
         if 'input_shape' not in kwargs and 'input_dim' in kwargs:
             kwargs['input_shape'] = (kwargs.pop('input_dim'),)
         super(ClusteringLayer, self).__init__(**kwargs)
-        self.n_clusters = n_clusters
+        self.num_clusters = num_clusters
         self.alpha = alpha
         self.initial_weights = weights
         self.input_spec = InputSpec(ndim=2)
@@ -120,7 +128,7 @@ class ClusteringLayer(Layer):
         input_dim = input_shape[1]
         self.input_spec = InputSpec(dtype=k.floatx(), shape=(None, input_dim))
         self.clusters = self.add_weight(
-            shape=(self.n_clusters, input_dim),
+            shape=(self.num_clusters, input_dim),
             initializer='glorot_uniform',
             name='clusters')
         if self.initial_weights is not None:
@@ -137,7 +145,8 @@ class ClusteringLayer(Layer):
         Arguments:
             inputs: the variable containing data, shape=(n_samples, n_features)
         Return:
-            q: student's t-distribution, or soft labels for each sample. shape=(n_samples, n_clusters)
+            q: student's t-distribution, or soft labels for each sample. 
+                shape=(n_samples, n_clusters)
         """
         q = 1.0 / (1.0 + (k.sum(k.square(k.expand_dims(inputs,
                    axis=1) - self.clusters), axis=2) / self.alpha))
@@ -148,17 +157,17 @@ class ClusteringLayer(Layer):
 
     def compute_output_shape(self, input_shape):
         assert input_shape and len(input_shape) == 2
-        return input_shape[0], self.n_clusters
+        return input_shape[0], self.num_clusters
 
     def get_config(self):
-        config = {'n_clusters': self.n_clusters}
+        config = {'n_clusters': self.num_clusters}
         base_config = super(ClusteringLayer, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
 
 
 # %%
-autoencoder, encoder = autoencoder_model(dims, init=init)
+autoencoder, encoder = autoencoder_model(dims, init_fn=init)
 
 
 
