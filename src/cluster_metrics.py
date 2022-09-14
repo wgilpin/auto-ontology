33184c60e6a2f36938e718026386bb7476ed0cc3
Will Gilpin <wgilpin@gmail.com>
2022-09-14 14:41:50 +0100

    moved do_clustering to cluster_metrics

--- src/cluster_metrics.py
+++ src/cluster_metrics.py
@@ -1,17 +1,20 @@
 import os
 import glob
 from collections import defaultdict
+from typing import Optional
 from pathlib import Path
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import sklearn.metrics
 from pandas import DataFrame, crosstab
 from wordcloud import WordCloud
 from sklearn.metrics import (
     f1_score, accuracy_score, precision_score, recall_score)
 from jinja2 import Environment, FileSystemLoader
-
+import sklearn.metrics
+from sklearn.cluster import KMeans, DBSCAN, OPTICS, AgglomerativeClustering
+from sklearn import mixture
+from scipy.stats import multivariate_normal
 
 from linear_assignment import linear_assignment
 from cluster import Cluster, ClusterList
@@ -23,9 +26,33 @@ def output(verbose, s: str) -> None:
     if verbose > 0:
         print(s)
 
+def cluster_acc(y_true, y_pred, y_pred_cluster):
+    """
+    Calculate clustering accuracy
+    """
+    y_true = y_true.astype(np.int64)
+    assert y_pred_cluster.size == y_true.size
+    D = max(y_pred_cluster.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred_cluster.size):
+        w[y_pred[i], y_true[i]] += 1
+    ind = linear_assignment(w.max() - w)
+    c_loss = sum([w[i, j] for i, j in ind]) * 1.0 / y_pred_cluster.size
+    print(f"Cluster Loss {c_loss} on {y_pred_cluster.size} clusters")
+    return c_loss
+    
+def cluster_loss(clustering:str, n_clusters: int):
+    """
+    Kearas loss function wrapper
+    """
+    def loss(y_true, y_pred):
+        y_pred_cluster, _ = do_clustering(clustering, n_clusters, y_pred)
+        return cluster_acc(y_true, y_pred, y_pred_cluster)
+    return loss
+
 def acc(y_true, y_pred):
     """
-    Calculate clustering accuracy. Require scikit-learn installed
+    Calculate clustering accuracy. 
 
     # Arguments
         y: true labels, numpy.array with shape `(n_samples,)`
@@ -50,7 +77,10 @@ def plot_confusion(
         save_dir: str,
         size: int=8,
         details: bool=True,
-        new_labels: dict = {}):
+        new_labels: Optional[dict] = None):
+    """
+    Plot a confusion matrix.
+    """
 
     sns.set(font_scale=3)
     confusion_matrix = sklearn.metrics.confusion_matrix(y, y_pred)
@@ -82,8 +112,6 @@ def plot_confusion(
     if save_dir:
         plt.savefig(os.path.join(save_dir, "confusion_matrix.png"))
     plt.show()
-
-
 
 def summarise_scores(scores: list[dict]) -> None:
     """
@@ -151,7 +179,6 @@ def freqs_descending(df, col):
 
 def rearrange_clusters(
                     sample,
-                    y_pred_sample,
                     mapping,
                     do_rearrange:bool=True,
                     ) -> tuple[DataFrame, ClusterList]:
@@ -170,11 +197,14 @@ def rearrange_clusters(
 
     clusters: ClusterList = {}
 
-    for clus_no in np.unique(y_pred_sample):
+    for clus_no in np.unique(sample['y_pred']): 
         if clus_no < 0:
             continue
         cluster = sample[sample['y_pred'] == clus_no]
         prob_ent = int(np.argmax(y_tru_per_clus[clus_no]))
+        if mapping[prob_ent] == 'UNKNOWN':
+            # get next best
+            prob_ent = list(np.argsort(y_tru_per_clus[clus_no]))[-2]
         prob_lbl = mapping[prob_ent]
         frac = y_tru_frac_by_clus[clus_no][prob_ent]
 
@@ -243,18 +273,18 @@ def show_wordcloud(
     else:
         print(f"No words for cluster {name}")
 
-def calc_metrics(TP, FP, FN):
+def calc_metrics(true_pos, false_pos, false_neg):
     """
     Calculate f1, precision and recall
     """
-    if TP + FP == 0:
+    if true_pos + false_pos == 0:
         precision = 0
     else:
-        precision = TP / (TP + FP)
-    if TP + FN == 0:
+        precision = true_pos / (true_pos + false_pos)
+    if true_pos + false_neg == 0:
         recall = 0
     else:
-        recall = TP / (TP + FN)
+        recall = true_pos / (true_pos + false_neg)
     if precision + recall == 0:
         f1 = 0
     else:
@@ -269,10 +299,6 @@ def eval_cluster(
     """
     show wordclouds for each cluster
     """
-    assert 'text' in sample.columns
-    assert 'y_true' in sample.columns
-    assert 'y_pred' in sample.columns
-    assert 'y_label' in sample.columns
 
     sample, clusters = rearrange_clusters(
                                     sample,
@@ -288,35 +314,35 @@ def eval_cluster(
         c_ent = ce.entity_id
 
         # the right entity class in the right cluster
-        TP = c[
+        true_pos = c[
             # in this cluster
             (c.y_pred_new == c_ent) &\
             # and this is the right class
             (c.y_true == c_ent)].shape[0]
 
         # this cluster, we think it's right entity but not the right entity
-        FP = c[
+        false_pos = c[
             # in this cluster
             (c.y_pred_new == c_ent) &\
             # but not the right entity class
             (c.y_true != c_ent)].shape[0]
 
         # it's the right entity in wrong cluster
-        FN = sample[
+        false_neg = sample[
             # not in this cluster
             (sample.y_pred_new != c_ent) &\
             # but should be
             (sample.y_true == c_ent)].shape[0]
 
-        f1, prec, rec = calc_metrics(TP, FP, FN)
+        f1, prec, rec = calc_metrics(true_pos, false_pos, false_neg)
 
         cluster_scores[cluster_name] = {
             'F1': f1,
             'precision': prec,
             'recall': rec,
-            'TP': TP,
-            'FP': FP,
-            'FN': FN,
+            'true_pos': true_pos,
+            'false_pos': false_pos,
+            'false_neg': false_neg,
         }
 
         if cluster_name[0:3] == 'UNK':
@@ -464,3 +490,126 @@ def save_scores(cluster_list, scores, save_dir, run_name):
         run_name,
         scores,
     )
+
+def do_clustering(
+        clustering: str,
+        n_clusters: int,
+        z_state: DataFrame,
+        params: Optional[dict] = None) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Perform clustering on the data.
+        -clustering: the clustering algorithm to use
+        -n_clusters: the number of clusters to use
+        -z_state: the data to cluster
+        -params: dict, optional
+            'eps' or 'min_samples' values for DBSCAN/OPTICS
+    Returns:
+        - the cluster assignments
+        - cluster centers
+    """
+    dbscan_eps = 1
+    dbscan_min_samples = 5
+    min_cluster_size = 15
+    if params is  None:
+        params = {}
+    if 'eps' in params:
+        dbscan_eps = params['eps']
+    if 'min_samples' in params:
+        dbscan_min_samples = params['min_samples']
+    if 'min_cluster_size' in params:
+        min_cluster_size = params['min_cluster_size']
+
+    if clustering == 'GMM':
+        gmix = mixture.GaussianMixture(
+            n_components=n_clusters,
+            covariance_type='full',
+            verbose=params['verbose'] if 'verbose' in params else 0)
+        gmix.fit(z_state)
+        y_pred = gmix.predict(z_state)
+        # get centres
+        centers = np.empty(shape=(gmix.n_components, z_state.shape[1]))
+        for i in range(gmix.n_components):
+            density = multivariate_normal(
+                cov=gmix.covariances_[i],
+                mean=gmix.means_[i]).logpdf(z_state)
+            centers[i, :] = z_state[np.argmax(density)]
+    elif clustering == 'Kmeans':
+        kmeans = KMeans(n_clusters=n_clusters, n_init=10,
+                        verbose=params['verbose'])
+        y_pred = kmeans.fit_predict(z_state)
+        centers = kmeans.cluster_centers_
+    elif clustering == 'DBSCAN':
+        dbscan = DBSCAN(
+            eps=dbscan_eps,
+            min_samples=dbscan_min_samples,
+            metric='manhattan')
+        y_pred = dbscan.fit_predict(z_state)
+        centers = np.zeros((len(np.unique(y_pred))))
+    elif clustering == 'OPTICS':
+        optics = OPTICS(
+            min_samples=dbscan_min_samples,
+            min_cluster_size=min_cluster_size,
+            metric='cosine')
+        y_pred = optics.fit_predict(z_state)
+        centers = np.zeros((len(np.unique(y_pred))))
+    elif clustering == "agg":
+        agg = AgglomerativeClustering(
+            n_clusters=n_clusters,
+            affinity='cosine',
+            linkage='complete')
+        y_pred = agg.fit_predict(z_state)
+        centers = None
+    else:
+        raise ValueError('Clustering algorithm not specified/unknown.')
+
+    return y_pred, centers  # type: ignore
+
+def do_evaluation(
+            sample: DataFrame,
+            mapping: dict,
+            verbose: int,
+            save_dir:str,
+            run_name:str) -> dict:
+    """
+    Evaluate the model.
+    - z_sample: the latent space
+    - y_sample: the true labels
+    - str_sample: the original strings
+    """
+    assert 'text' in sample.columns
+    assert 'y_true' in sample.columns
+    assert 'y_pred' in sample.columns
+
+    if verbose>0:
+        print("CLUSTERS")
+
+    # assign labels to the clusters
+    assert mapping is not None
+    all_clusters, clusters, cluster_f1 = eval_cluster(
+                                            sample, mapping)
+
+    # list of new clusters by id number
+    labels_no = {v.clus_no: k for k,v in clusters.items()}
+    labels_ent = {v.entity_id: k for k,v in clusters.items()}
+    new_labels = {**labels_no, **labels_ent}
+
+    # overall scores
+    scores_agg = show_core_metrics(
+        sample.y_pred,
+        all_clusters,
+        new_labels, mapping,
+        sample.y_true,
+        save_dir)
+
+    # cluster scores
+    cluster_list = score_clusters(
+                            clusters=clusters,
+                            save_dir=save_dir,
+                            mapping=mapping)
+
+    # output file
+    save_scores(cluster_list, scores_agg, save_dir, run_name)
+
+    scores = {**scores_agg, 'cluster F1': cluster_f1} # type: ignore
+
+    return scores
